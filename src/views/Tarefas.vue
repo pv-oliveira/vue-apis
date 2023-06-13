3c576d0e55c24fb56a8c20abba87d9729fd177c5
Paulo Victor <Paulo.victor.ig@hotmail.com>
2023-06-13 17:01:11 -0300

    utlizando o hook computed foi criado um filtro no componente tarefas

--- src/views/Tarefas.vue
+++ src/views/Tarefas.vue
@@ -1,39 +1,42 @@
 <template>
-    <Formulario @aoSalvarTarefa="salvarTarefa"/>
-    <div class="lista">
-        <Box v-if="semTarefas">
-            Você não está muito produtivo hoje <span class="has-text-weight-bold">:</span>
-        </Box>
-        <Tarefa v-for="(tarefa, index) in tarefas" :tarefa="tarefa" :key="index" @aoTarefaClicada="selecionarTarefa"/>
-        <div class="modal" :class="{ 'is-active': tarefaSelecionada }" v-if="tarefaSelecionada">
-        <div class="modal-background"></div>
-        <div class="modal-card">
-          <header class="modal-card-head">
-            <p class="modal-card-title">Editando tarefas</p>
-            <button @click="fecharModal" class="delete" aria-label="close"></button>
-          </header>
-          <section class="modal-card-body">
-            <div class="field">
+  <Formulario @aoSalvarTarefa="salvarTarefa" />
+  <div class="lista">
+    <Box v-if="semTarefas">
+      Você não está muito produtivo hoje <span class="has-text-weight-bold">:</span>
+    </Box>
+    <div class="field">
+      <p class="control has-icons-left">
+        <input class="input" type="text" placeholder="Digite para filtrar" v-model="filtro">
+        <span class="icon is-small is-left">
+          <i class="fas fa-search"></i>
+        </span>
+      </p>
+    </div>
+    <Tarefa v-for="(tarefa, index) in tarefas" :tarefa="tarefa" :key="index" @aoTarefaClicada="selecionarTarefa" />
+    <div class="modal" :class="{ 'is-active': tarefaSelecionada }" v-if="tarefaSelecionada">
+      <div class="modal-background"></div>
+      <div class="modal-card">
+        <header class="modal-card-head">
+          <p class="modal-card-title">Editando tarefas</p>
+          <button @click="fecharModal" class="delete" aria-label="close"></button>
+        </header>
+        <section class="modal-card-body">
+          <div class="field">
             <label for="descricaoDaTarefa" class="label"> Descrição </label>
-            <input
-              type="text"
-              class="input"
-              v-model="tarefaSelecionada.descricao"
-              id="descricaoDaTarefa"
-            />
+            <input type="text" class="input" v-model="tarefaSelecionada.descricao" id="descricaoDaTarefa" />
           </div>
-          </section>
-          <footer class="modal-card-foot">
-            <button @click="alterarTarefa" class="button is-success">Salvar</button>
-            <button @click="fecharModal" class="button">Cancelar</button>
-          </footer>
-        </div>
+        </section>
+        <footer class="modal-card-foot">
+          <button @click="alterarTarefa" class="button is-success">Salvar</button>
+          <button @click="fecharModal" class="button">Cancelar</button>
+        </footer>
       </div>
     </div>
+  </div>
 </template>
 
 <script lang="ts">
-import { computed, defineComponent } from "vue";
+import { computed, defineComponent, ref, watchEffect } from "vue";
 import Formulario from "../components/Formulario.vue";
 import Tarefa from "../components/Tarefa.vue";
 import Box from "../components/Box.vue";
@@ -55,7 +58,7 @@ export default defineComponent({
     }
   },
   methods: {
-    salvarTarefa (tarefa:ITarefa) : void {
+    salvarTarefa(tarefa: ITarefa): void {
       this.store.dispatch(CADASTRAR_TAREFA, tarefa)
     },
     selecionarTarefa(tarefa: ITarefa) {
@@ -66,22 +69,31 @@ export default defineComponent({
     },
     alterarTarefa() {
       this.store.dispatch(ALTERAR_TAREFA, this.tarefaSelecionada)
-       .then(() => this.fecharModal())
+        .then(() => this.fecharModal())
     }
   },
   computed: {
-    semTarefas () :boolean {
+    semTarefas(): boolean {
       return this.tarefas.length == 0
     }
   },
-  setup () {
+  setup() {
     const store = useStore()
     store.dispatch(OBTER_TAREFAS)
     store.dispatch(OBTER_PROJETOS)
 
+    const filtro = ref("");
+
+    // const tarefas = computed(() => store.state.tarefas.filter(t => !filtro.value || t.descricao.includes(filtro.value)));
+
+    watchEffect(() => {
+      store.dispatch(OBTER_TAREFAS, filtro.value)
+    })
+
     return {
       tarefas: computed(() => store.state.tarefas),
-      store
+      store,
+      filtro
     }
   }
 });
